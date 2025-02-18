165b920054d6169ebfc1857341b0108c6132ed8b
Cesar Bautista <bautistac.cesar.p8@gmail.com>
2025-02-18 01:05:44 -0600

    Incorporacion de BeautifulSoup

--- horarios/views.py
+++ horarios/views.py
@@ -15,37 +15,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import re
+import requests
+from bs4 import BeautifulSoup
     
 # Pagina principal de horarios, manda los grupos y materias que un usuario autenticado a guardado
 def horarios(request):
-    # import requests
-    # from bs4 import BeautifulSoup
-
-    # # URL de la página
-    # url = "https://www.ssa.ingenieria.unam.mx/cj/tmp/programacion_horarios/1414.html"
-
-    # # Simular un navegador con un User-Agent válido
-    # headers = {
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
-    # }
-
-    # # Hacer la petición GET
-    # response = requests.get(url, headers=headers)
-
-    # # Verificar si la solicitud fue exitosa (código 200)
-    # if response.status_code == 200:
-    #     # Analizar el HTML con BeautifulSoup
-    #     soup = BeautifulSoup(response.text, "html.parser")
-
-    #     # Imprimir el HTML completo (opcional)
-    #     print(soup.prettify())
-
-    #     # Si quieres obtener solo el texto sin etiquetas HTML
-    #     print(soup.get_text())
-
-    # else:
-    #     print(f"Error {response.status_code}: No se pudo acceder a la página")
-
+    
     if request.user.is_authenticated:
         try:
             materias_encontradas = MateriasAlumno.objects.filter(alumno=request.user).values_list('materias', flat=True)
@@ -194,77 +169,104 @@ def guardarGrupos(request):
 
 # Funcion para actualizar los datos de una materia
 def actualizarMateria(request, id):
-    try:
-        materia = Materia.objects.get(clave=id)
-        print(f"Actualizando materia con ID: {materia.id}")
-        opts = Options()
-        opts.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
-        opts.add_argument("--disable-search-engine-choice-screen")
-        opts.add_argument("--disable-logging")
-        opts.add_argument("--log-level=3")
-        opts.add_argument("--disable-gpu")
-        opts.add_argument("--window-size=1920,1080")
-        opts.add_argument("--headless")
-        driver = webdriver.Chrome(options=opts)
-        driver.implicitly_wait(5)
-        driver.get('https://www.ssa.ingenieria.unam.mx/horarios.html')
-        driver.find_element(By.XPATH, "//input[@id='clave']").send_keys(str(materia.clave))
-        driver.find_element(By.XPATH, "//button[@id='buscar']").click()
-        clases = driver.find_elements(By.XPATH, '//tbody')[1:]  
-        for clase in clases:
-            if not clase.text.strip():
-                continue
-            try:
-                datos = clase.find_elements(By.XPATH, './tr/td')
-                texto_celdas = [dato.text for dato in datos]
-                texto_celdas[2] = re.sub(r'\n.*', '', texto_celdas[2]).strip()
-                grupo_existente = Grupo.objects.filter(
-                    materia=materia,
-                    grupo=int(texto_celdas[1]),
-                ).first()
-                if grupo_existente:  
-                    grupo_existente.grupo=int(texto_celdas[1])
-                    grupo_existente.nombre=texto_celdas[2]
-                    grupo_existente.tipo=texto_celdas[3]
-                    grupo_existente.horas=texto_celdas[4]
-                    grupo_existente.dias=texto_celdas[5]
-                    grupo_existente.salon=texto_celdas[6]
-                    grupo_existente.cupo=int(texto_celdas[8])
-                    print(f"Calificacion: {calificacionProfesor(texto_celdas[2])}")
-                    if(grupo_existente.calificacion == 0.0):
-                        grupo_existente.calificacion = calificacionProfesor(texto_celdas[2])
-                    grupo_existente.save()  
-                    print(f"Cupo actualizado para el grupo {texto_celdas[1]} de {materia.nombre}.")
-                else:
-                    Grupo.objects.create(
-                        materia=Materia.objects.get(clave=int(texto_celdas[0])),
-                        grupo=texto_celdas[1],
-                        nombre=texto_celdas[2],
-                        tipo=texto_celdas[3],
-                        horas=texto_celdas[4],
-                        dias=texto_celdas[5],
-                        salon=texto_celdas[6],
-                        cupo=int(texto_celdas[8]),
-                        calificacion=calificacionProfesor(texto_celdas[2])
-                    )
-                    print(f"Se añadio el grupo {texto_celdas[1]} de {materia.nombre}.")
-            except Exception as e:
-                print(f"Error al procesar la clase: {e}")
-                continue
+    materia = Materia.objects.get(clave=id)
+    print(f"Actualizando materia con ID: {materia.id}")
+
+    url = f"https://www.ssa.ingenieria.unam.mx/cj/tmp/programacion_horarios/{id}.html"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        table = soup.find('table', class_='table table-horarios-custom')
+
+        if not table:
+            return JsonResponse({'message': 'No se encontró la tabla de horarios.'}, status=404)
+
+        tbodies = table.find_all('tbody')[1:]
+
+        grupos_actualizados = []
+
+        for tbody in tbodies:
+            filas = tbody.find_all('tr')
+
+            for fila in filas:
+                try:
+                    datos = fila.find_all('td')
+                    texto_celdas = [dato.text.strip() for dato in datos]
+
+                    if len(texto_celdas) < 9: 
+                        print("Fila con datos incompletos, omitiendo:", texto_celdas)
+                        continue
+
+                    texto_celdas[2] = re.sub(r'\n.*', '', texto_celdas[2]).strip()
+
+                    grupo_existente = Grupo.objects.filter(
+                        materia=materia,
+                        grupo=int(texto_celdas[1]),
+                    ).first()
+
+                    if grupo_existente:
+                        grupo_existente.grupo = int(texto_celdas[1])
+                        grupo_existente.nombre = texto_celdas[2]
+                        grupo_existente.tipo = texto_celdas[3]
+                        grupo_existente.horas = texto_celdas[4]
+                        grupo_existente.dias = texto_celdas[5]
+                        grupo_existente.salon = texto_celdas[6]
+                        grupo_existente.cupo = int(texto_celdas[8])
+
+                        # if grupo_existente.calificacion == 0.0:
+                        #     grupo_existente.calificacion = calificacionProfesor(texto_celdas[2])
+
+                        grupo_existente.save()
+                        print(f"Cupo actualizado para el grupo {texto_celdas[1]} de {materia.nombre}.")
+                    else:
+                        grupo_existente = Grupo.objects.create(
+                            materia=materia,
+                            grupo=int(texto_celdas[1]),
+                            nombre=texto_celdas[2],
+                            tipo=texto_celdas[3],
+                            horas=texto_celdas[4],
+                            dias=texto_celdas[5],
+                            salon=texto_celdas[6],
+                            cupo=int(texto_celdas[8]),
+                            calificacion=calificacionProfesor(texto_celdas[2])
+                        )
+                        print(f"Se añadió el grupo {texto_celdas[1]} de {materia.nombre}.")
+
+                    grupos_actualizados.append({
+                        'grupo': grupo_existente.grupo,
+                        'nombre': grupo_existente.nombre,
+                        'tipo': grupo_existente.tipo,
+                        'horas': grupo_existente.horas,
+                        'dias': grupo_existente.dias,
+                        'salon': grupo_existente.salon,
+                        'cupo': grupo_existente.cupo,
+                        'calificacion': grupo_existente.calificacion,
+                    })
+
+                except Exception as e:
+                    print(f"Error al procesar la clase: {e}")
+                    continue
+
         materia_data = {
-                        'clave': materia.clave,
-                        'nombre': materia.nombre,
-                        'color': materia.color.color,
-                    }
-        grupos = Grupo.objects.filter(materia=materia)
-        grupos_data = list(grupos.values())
-        materiaNueva = {id: {'grupos': grupos_data, 'materia': materia_data}}
-        driver.quit()
+            'clave': materia.clave,
+            'nombre': materia.nombre,
+            'color': materia.color.color,
+        }
+
+        materiaNueva = {id: {'grupos': grupos_actualizados, 'materia': materia_data}}
+
         return JsonResponse({'message': 'Materia Actualizada', 'materiaNueva': materiaNueva})
-    except Materia.DoesNotExist:
-        print(f"No se encontró la materia con clave {id}")
-        return redirect('horarios')
-    
+
+    else:
+        print(f"Error {response.status_code}: No se pudo acceder a la página")
+        return JsonResponse({'message': 'No se pudo acceder a la página', 'status': response.status_code}, status=response.status_code)
+
 
 
 #Funcion para la configuracion del navegador en selenium
